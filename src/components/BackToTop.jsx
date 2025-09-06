1b5e7ffacadf569e2725a046775c0e6c9a44c249
m-afnan2018 <m.afnan2018@gmail.com>
2025-09-06 21:17:29 +0530

    Completed

--- src/components/BackToTop.jsx
+++ src/components/BackToTop.jsx
@@ -36,9 +36,9 @@ export default function BackToTop() {
 
     return (
         // Render the button with a conditional className based on the visible state
-        <a className={`back-to-top ${visible ? 'd-block' : 'd-none'}`} onClick={handleClick}>
+        <span className={`back-to-top ${visible ? 'd-block' : 'd-none'}`} onClick={handleClick}>
             <i className="fa fa-chevron-up">
             </i>
-        </a>
+        </span>
     );
 }
--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -9,14 +9,14 @@ export default function Footer() {
                         <div className="footer-contact">
                             <h2>Contact Us</h2>
                             <a href="https://maps.app.goo.gl/HQg6gMPpAJ445cda6" target="_blank" rel="noopener noreferrer"><p><i className="fa fa-map-marker-alt"></i>Patna, India</p></a>
-                            <a href="tel:+91-9065382715"><p><i className="fa fa-phone"></i>+91-9065382715</p></a>
-                            <a href="mailto:info.nts.pvt@gmail.com"><p><i className="fa fa-envelope"></i>info.nts.pvt@gmail.com</p></a>
+                            <a href="tel:+91-9065382715" target="_blank" rel="noopener noreferrer"><p><i className="fa fa-phone"></i>+91-9065382715</p></a>
+                            <a href="mailto:info.nts.pvt@gmail.com" target="_blank" rel="noopener noreferrer"><p><i className="fa fa-envelope"></i>info.nts.pvt@gmail.com</p></a>
                             <div className="footer-social">
-                                <a className="btn btn-custom" href="https://twitter.com/vit_socials"><i className="fab fa-x-twitter"></i></a>
-                                <a className="btn btn-custom" href="https://www.facebook.com/vitsocials"><i className="fab fa-facebook-f"></i></a>
-                                <a className="btn btn-custom" href="https://www.instagram.com/vitsocials/"><i className="fab fa-instagram"></i></a>
-                                <a className="btn btn-custom" href="https://www.youtube.com/channel/UCJnaNm8Ns08rUIhsdFM2fhA"><i className="fab fa-youtube"></i></a>
-                                <a className="btn btn-custom" href="https://www.linkedin.com/company/vit-social-welfare-development/"><i className="fab fa-linkedin-in"></i></a>
+                                <a className="btn btn-custom"  target="_blank" rel="noopener noreferrer" href="https://twitter.com/vit_socials"><i className="fab fa-x-twitter"></i></a>
+                                <a className="btn btn-custom"  target="_blank" rel="noopener noreferrer" href="https://www.facebook.com/vitsocials"><i className="fab fa-facebook-f"></i></a>
+                                <a className="btn btn-custom"  target="_blank" rel="noopener noreferrer" href="https://www.instagram.com/vitsocials/"><i className="fab fa-instagram"></i></a>
+                                <a className="btn btn-custom"  target="_blank" rel="noopener noreferrer" href="https://www.youtube.com/channel/UCJnaNm8Ns08rUIhsdFM2fhA"><i className="fab fa-youtube"></i></a>
+                                <a className="btn btn-custom"  target="_blank" rel="noopener noreferrer" href="https://www.linkedin.com/company/vit-social-welfare-development/"><i className="fab fa-linkedin-in"></i></a>
                             </div>
                         </div>
                     </div>

69dbca96dbaec09e10f5d13a3e685389088902cc
m-afnan2018 <m.afnan2018@gmail.com>
2025-09-06 20:29:30 +0530

    First

---
+++ src/components/Navbar.jsx
@@ -0,0 +1,86 @@
+import { useEffect} from 'react';
+import { Link, useLocation } from 'react-router-dom'
+import '../css/navbar.css';
+
+export default function Navbar() {
+    const path = useLocation().pathname;
+
+    useEffect(() => {
+        window.scrollTo({ top: 0, behavior: 'smooth' });
+        // Get the top bar element
+        const topBar = document.querySelector('.top-bar');
+        // Get the navbar element
+        const navbar = document.querySelector('.navbar');
+        function toggleStickyNavbar() {
+            if (window.scrollY > topBar.offsetHeight) {
+                navbar.classList.add('nav-sticky');
+                topBar.classList.add('hide-top-bar'); // Add class to hide top bar
+            } else {
+                navbar.classList.remove('nav-sticky');
+                topBar.classList.remove('hide-top-bar'); // Remove class to show top bar
+            }
+        }
+        window.addEventListener('scroll', toggleStickyNavbar);
+    }, [])
+
+
+    return (
+        <>
+            {/* <!-- Top Bar Start --> */}
+            <div className="top-bar d-none d-md-block">
+                <div className="container-fluid">
+                    <div className="row">
+                        <div className="col-md-8">
+                            <div className="top-bar-left">
+                                {/* <div className='text'>
+                                    <Link to='/admin'><i title='ADMIN' className='fa fa-solid fa-user-tie m-0' style={{ fontSize: "21px" }} /></Link>
+                                </div> */}
+                                <div className="text">
+                                    <i className="fa fa-phone"></i>
+                                    <a href="tel:+91-9065382715"><p>+91-9065382715</p></a>
+                                </div>
+                                <div className="text">
+                                    <i className="fa fa-envelope"></i>
+                                    <a href="mailto:info.nts.pvt@gmail.com"><p>info.nts.pvt@gmail.com</p></a>
+                                </div>
+                            </div>
+                        </div>
+                        <div className="col-md-4">
+                            <div className="top-bar-right">
+                                <div className="social">
+                                    <a href="https://twitter.com/vit_socials"><i className="fab fa-x-twitter"></i></a>
+                                    <a href="https://www.facebook.com/vitsocials"><i className="fab fa-facebook-f"></i></a>
+                                    <a href="https://www.instagram.com/vitsocials/"><i className="fab fa-instagram"></i></a>
+                                    <a href="https://www.youtube.com/channel/UCJnaNm8Ns08rUIhsdFM2fhA"><i className="fab fa-youtube"></i></a>
+                                    <a href="https://www.linkedin.com/company/vit-social-welfare-development/"><i className="fab fa-linkedin-in"></i></a>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </div>
+            {/* <!-- Top Bar End --> */}
+
+            {/* <!-- Nav Bar Start --> */}
+            <div className="navbar navbar-expand-lg bg-dark navbar-dark">
+                <div className="container-fluid">
+                    <Link to="/" className="navbar-brand">OM TARA FOUNDATION</Link>
+                    <button type="button" className="navbar-toggler" data-bs-toggle="collapse" data-bs-target="#navbarCollapse">
+                        <span className="navbar-toggler-icon"></span>
+                    </button>
+
+                    <div className="collapse navbar-collapse justify-content-between" id="navbarCollapse">
+                        <div className="navbar-nav ml-auto">
+                            <Link to="/" className={`nav-item nav-link ${path === '/' ? "active" : ""}`}>Home</Link>
+                            <Link to="/about" className={`nav-item nav-link ${path === '/about' ? "active" : ""}`}>About</Link>
+                            <Link to="/event" className={`nav-item nav-link ${path === '/event' ? "active" : ""}`}>Events</Link>
+                            <Link to="/contact" className={`nav-item nav-link ${path === '/contact' ? "active" : ""}`}>Contact</Link>
+                            <Link to="https://rzp.io/rzp/D0CE9EJ5" target="_blank" className={`nav-item nav-link ${path === '/donate' ? "active" : ""}`}>Donate</Link>
+                        </div>
+                    </div>
+                </div>
+            </div>
+            {/* <!-- Nav Bar End --> */}
+        </>
+    );
+}
